5921b8fc310bed52c94a4245c49e7be9b51b8d65
xiaotao0110 <2430053431@qq.com>
2020-01-12 15:50:16 +0800

    修复bug

--- crtvn-center/src/main/java/com/hnieu/crtvn/controller/sys/TokenController.java
+++ crtvn-center/src/main/java/com/hnieu/crtvn/controller/sys/TokenController.java
@@ -2,6 +2,7 @@ package com.hnieu.crtvn.controller.sys;
 
 import com.hnieu.crtvn.util.CookieUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,13 +25,15 @@ public class TokenController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${oauth.token.url}")
+    private String baseUrl;
 
     //用户认证申请令牌，将令牌存储到redis
     @PostMapping("/applyToken")
     public Map<String, Object> login(String username, String password, String clientId, String clientSecret) {
 
-        String url = "http://127.0.0.1:1001/oauth/token?" +
-                "grant_type=password" +
+        String url = baseUrl +
+                "?grant_type=password" +
                 "&username=" + username +
                 "&password=" + password +
                 "&client_id=" + clientId +
