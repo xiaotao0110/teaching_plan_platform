de0ab1e2d6fc96d8e89d7e83efdde8c80bc8035a
xiaotao0110 <2430053431@qq.com>
2020-01-19 17:42:45 +0800

    修复bug

--- crtvn-center/src/main/resources/static/js/crtvn/common/config.js
+++ crtvn-center/src/main/resources/static/js/crtvn/common/config.js
@@ -2,6 +2,6 @@
 // var base_url = " http://127.0.0.1:1005/";
 // var base_code_url = " http://127.0.0.1:1003/";
 
-dev
+//dev
 var base_url = " http://192.168.254.150:1005/";
 var base_code_url = " http://192.168.254.150:1003/";
