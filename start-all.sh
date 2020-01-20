850e3891122ead871cc13018006aabc798e1a922
xiaotao0110 <2430053431@qq.com>
2020-01-20 14:07:48 +0800

    优化启动脚本

--- start-all.sh
+++ start-all.sh
@@ -20,6 +20,9 @@ docker rm register-center
 docker rmi teaching/register-center:latest
 mvn clean install  package docker:build -Dmaven.test.skip=true
 echo "------------------构建register结束"
+echo "------------------启动register容器"
+docker run -id  --name=register-center  -p 1000:1000 teaching/register-center:latest /bin/bash
+
 
 echo "------------------构建api-gateway镜像"
 cd   $WORKSPACE/api-gateway
@@ -28,6 +31,8 @@ docker rm api-gateway
 docker rmi teaching/api-gateway:latest
 mvn clean install  package docker:build -Dmaven.test.skip=true
 echo "------------------构建api-gateway结束"
+echo "------------------启动api-gateway容器"
+docker run -id  --name=api-gateway  -p 1005:1005   teaching/api-gateway:latest /bin/bash
 
 echo "------------------构建oauth镜像"
 cd   $WORKSPACE/oauth-center
@@ -36,6 +41,8 @@ docker rm oauth-center
 docker rmi teaching/oauth-center:latest
 mvn clean install  package docker:build -Dmaven.test.skip=true
 echo "------------------构建oauth结束"
+echo "------------------启动oauth容器"
+docker run -id  --name=oauth-center  -p 1001:1001  teaching/oauth-center:latest /bin/bash
 
 echo "------------------构建crtvn镜像"
 cd   $WORKSPACE/crtvn-center
@@ -44,24 +51,16 @@ docker rm crtvn-center
 docker rmi teaching/crtvn-center:latest
 mvn clean install  package docker:build -Dmaven.test.skip=true
 echo "------------------构建crtvn结束"
-
-echo "------------------构建镜像完成------------------"
-
-echo "-----------------------------------------------"
-
-echo "------------------开始启动容器------------------"
-
-
-echo "------------------启动register容器"
-docker run -id  --name=register-center  -p 1000:1000 teaching/register-center:latest /bin/bash
-
-echo "------------------启动api-gateway容器"
-docker run -id  --name=api-gateway  -p 1005:1005   teaching/api-gateway:latest /bin/bash
-
-echo "------------------启动oauth容器"
-docker run -id  --name=oauth-center  -p 1001:1001  teaching/oauth-center:latest /bin/bash
-
 echo "------------------启动crtvn容器"
 docker run -id  --name=crtvn-center  -p 1003:1003  teaching/crtvn-center:latest /bin/bash
+
+
+
+
+
+
+
+
+
 
 echo "------------------启动容器完成------------------"
