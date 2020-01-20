6072347015abb34d1d91e2826c7da8bb56062c85
xiaotao0110 <2430053431@qq.com>
2020-01-20 13:57:20 +0800

    修复bug

--- start-all.sh
+++ start-all.sh
@@ -53,15 +53,15 @@ echo "------------------开始启动容器------------------"
 
 
 echo "------------------启动register容器"
-docker run -id  --name=register-center  -p 1000:1000  -d  teaching/register-center:latest /bin/bash
+docker run -id  --name=register-center  -p 1000:1000 teaching/register-center:latest /bin/bash
 
 echo "------------------启动api-gateway容器"
-docker run -id  --name=api-gateway  -p 1005:1005  -d  teaching/api-gateway:latest /bin/bash
+docker run -id  --name=api-gateway  -p 1005:1005   teaching/api-gateway:latest /bin/bash
 
 echo "------------------启动oauth容器"
-docker run -id  --name=oauth-center  -p 1001:1001  -d  teaching/oauth-center:latest /bin/bash
+docker run -id  --name=oauth-center  -p 1001:1001  teaching/oauth-center:latest /bin/bash
 
 echo "------------------启动crtvn容器"
-docker run -id  --name=crtvn-center  -p 1003:1003  -d  teaching/crtvn-center:latest /bin/bash
+docker run -id  --name=crtvn-center  -p 1003:1003  teaching/crtvn-center:latest /bin/bash
 
 echo "------------------启动容器完成------------------"
